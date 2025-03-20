b322ad2478e8971f5848abcd28f5d5b20c563f93
Jaka <jaka.kocar@gmail.com>
2025-03-20 15:13:33 +0100

    Ustvarjena funckija za binary radix algoritem

--- dn1.cpp
+++ dn1.cpp
@@ -19,12 +19,18 @@ bool Branje_Stevil(vector<unsigned char> &A, const char s[]) {
     return true;
 }
 
+void Binary_Radix_Sort(unsigned char* A, unsigned int size) {
+
+}
+
 int main(int argc, const char* argv[]) {
     vector<unsigned char> A;
 
     if(argc < 2) return 0;
 
-    if(!Branje_Stevil(A, argv[2])) return 0;
+    if(!Branje_Stevil(A, argv[1])) return 0;
+    else
+        Binary_Radix_Sort(&A[0], A.size());
 
     return 0;
 }
