9310e92fb78fbd5f9c391118ece545fda557ca82
Jaka <jaka.kocar@gmail.com>
2025-03-20 14:27:32 +0100

    Priprava skripte, funkcija main

---
+++ dn1.cpp
@@ -0,0 +1,13 @@
+#include <fstream>
+#include <vector>
+using namespace std;
+
+//TODO: Funkcija za branje iz vhodne datoteke
+
+int main(int argc, const char* argv[]) {
+    vector<unsigned char> A;
+
+    if(argc < 2) return 0;
+
+    return 0;
+}
