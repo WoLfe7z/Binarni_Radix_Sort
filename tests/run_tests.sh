a0b6b6815ff7273870f6e9a60597ad4f648bb162
Jaka <jaka.kocar@gmail.com>
2025-04-05 15:59:25 +0200

    Popravljene poti do test datotek

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -2,8 +2,8 @@
 
 #Test 1
 echo "Izvajam test 1..."
-./dn1 test1_input.txt
-diff -q out.txt test1_expected.txt
+./dn1 tests/test1_input.txt
+diff -q out.txt tests/test1_expected.txt
 if [ $? -eq 0 ]; then
     echo "Test 1 je uspel!"
 else
@@ -13,8 +13,8 @@ fi
 
 #Test 2
 echo "Izvajam test 2..."
-./dn1 test2_input.txt
-diff -q out.txt test2_expected.txt
+./dn1 tests/test2_input.txt
+diff -q out.txt tests/test2_expected.txt
 if [ $? -eq 0 ]; then
     echo "Test 2 je uspel!"
 else
@@ -24,8 +24,8 @@ fi
 
 #Test 3
 echo "Izvajam test 3..."
-./dn1 test3_input.txt
-diff -q out.txt test3_expected.txt
+./dn1 tests/test3_input.txt
+diff -q out.txt tests/test3_expected.txt
 if [ $? -eq 0 ]; then
     echo "Test 3 je uspel!"
 else
