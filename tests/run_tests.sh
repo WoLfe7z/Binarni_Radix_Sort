12aa3130bb6631f25ca1976e357f0e79d32da4de
Jaka <jaka.kocar@gmail.com>
2025-04-05 16:09:01 +0200

    Popravljen diff ukaz

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -3,7 +3,7 @@
 #Test 1
 echo "Izvajam test 1..."
 ./dn1 tests/test1_input.txt
-diff -q out.txt tests/test1_expected.txt
+diff -w out.txt tests/test1_expected.txt
 if [ $? -eq 0 ]; then
     echo "Test 1 je uspel!"
 else
@@ -14,7 +14,7 @@ fi
 #Test 2
 echo "Izvajam test 2..."
 ./dn1 tests/test2_input.txt
-diff -q out.txt tests/test2_expected.txt
+diff -w out.txt tests/test2_expected.txt
 if [ $? -eq 0 ]; then
     echo "Test 2 je uspel!"
 else
@@ -25,7 +25,7 @@ fi
 #Test 3
 echo "Izvajam test 3..."
 ./dn1 tests/test3_input.txt
-diff -q out.txt tests/test3_expected.txt
+diff -w out.txt tests/test3_expected.txt
 if [ $? -eq 0 ]; then
     echo "Test 3 je uspel!"
 else
