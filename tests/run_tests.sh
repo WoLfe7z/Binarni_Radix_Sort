d4356ee9389431185e21884f4f65cdfd97d0b520
Jaka <jaka.kocar@gmail.com>
2025-04-05 17:47:47 +0200

    Dodajanje komentarjev

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,34 +1,15 @@
 #!/bin/bash
 
-#Test 1
-echo "Izvajam test 1..."
-./dn1 tests/test1_input.txt
-diff -w out.txt tests/test1_expected.txt
-if [ $? -eq 0 ]; then
-    echo "Test 1 je uspel!"
-else
-    echo "Test 1 je spodletel!"
-    exit 1
-fi
+TEST=$1
 
-#Test 2
-echo "Izvajam test 2..."
-./dn1 tests/test2_input.txt
-diff -w out.txt tests/test2_expected.txt
-if [ $? -eq 0 ]; then
-    echo "Test 2 je uspel!"
-else
-    echo "Test 2 je spodletel!"
-    exit 1
-fi
-
-#Test 3
-echo "Izvajam test 3..."
-./dn1 tests/test3_input.txt
-diff -w out.txt tests/test3_expected.txt
-if [ $? -eq 0 ]; then
-    echo "Test 3 je uspel!"
-else
-    echo "Test 3 je spodletel!"
-    exit 1
-fi
+for input_file in tests/*_input.txt; do
+	echo "Izvajam ${TEST}..."
+    	./dn1 tests/${TEST}_input.txt
+    	diff -w out.txt tests/${TEST}_expected.txt
+    	if [ $? -eq 0 ]; then
+	    echo "$TEST je uspel!"
+	else
+	    echo "$TEST je spodletel!"
+	    exit 1
+	fi
+done
